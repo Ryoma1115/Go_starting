ec4da5ca5506f0e4faea6242e3f19b751504b2ee
github Ryoma1115 <t9831703@gmail.com>
2020-09-27 22:56:57 +0900

    Add main.go.

---
+++ zoo/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"./animals"
+)
+
+fucn main() {
+	fmt.Println(animals.ElephantFeed())
+	fmt.Println(animals.MonkeyFeed())
+	fmt.Println(animals.RabbitFeed())
+}
