18445f42f5063728475c98dc29c611b9710e38db
github Ryoma1115 <t9831703@gmail.com>
2020-10-06 16:39:27 +0900

    Add 4.3.go.

---
+++ chapter4/4.3.go
@@ -0,0 +1,16 @@
+package main
+
+// func main() {
+// 	s := make([]int, 10)
+// 	fmt.Println(s)
+// }
+
+// func main() {
+// 	var a [10]int
+// 	fmt.Println(a)
+// }
+
+func main() {
+	s := make([]int, 8)
+	len(s)
+}
