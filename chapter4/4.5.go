b449e72d7afa742e8f36f0c96e5c9764c9c609dc
github Ryoma1115 <t9831703@gmail.com>
2020-10-07 18:09:22 +0900

    Modified 4.5.go.

--- chapter4/4.5.go
+++ chapter4/4.5.go
@@ -1,22 +1,57 @@
+// package main
+
+// import "fmt"
+
+// func receiver(ch <-chan int) {
+// 	for {
+// 		i := <-ch
+// 		fmt.Println(i)
+// 	}
+// }
+
+// func main() {
+// 	ch := make(chan int)
+
+// 	go receiver(ch)
+
+// 	i := 0
+// 	for i < 10000 {
+// 		ch <- i
+// 		i++
+// 	}
+// }
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func receiver(ch <-chan int) {
+func receive(name string, ch <-chan int) {
 	for {
-		i := <-ch
-		fmt.Println(i)
+		i, ok := <-ch
+		if ok == false {
+			break
+		}
+		fmt.Println(name, i)
 	}
+	fmt.Println(name + " is done.")
 }
 
 func main() {
-	ch := make(chan int)
+	ch := make(chan int, 20)
 
-	go receiver(ch)
+	go receive("1st goroutine", ch)
+	go receive("2st goroutine", ch)
+	go receive("3st goroutine", ch)
 
 	i := 0
-	for i < 10000 {
+	for i < 100 {
 		ch <- i
 		i++
 	}
+	close(ch)
+
+	time.Sleep(3 * time.Second)
 }
