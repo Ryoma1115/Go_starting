197519995036f4259c2f99024d2ce539f1307db3
github Ryoma1115 <t9831703@gmail.com>
2020-10-10 20:26:33 +0900

    Modified 5.3.go.

--- chapter5/5.3.go
+++ chapter5/5.3.go
@@ -2,16 +2,31 @@ package main
 
 import "fmt"
 
-type T struct {
-	N uint
-	_ int16
-	S []string
+// import "fmt"
+
+// type T struct {
+// 	N uint
+// 	_ int16
+// 	S []string
+// }
+
+// func main() {
+// 	t := T{
+// 		N: 12,
+// 		S: []string{"A", "B", "C"},
+// 	}
+// 	fmt.Println(t)
+// }
+
+type Point struct {
+	X, Y int
+}
+
+func showStruct(s struct{ X, Y int }) {
+	fmt.Println(s)
 }
 
 func main() {
-	t := T{
-		N: 12,
-		S: []string{"A", "B", "C"},
-	}
-	fmt.Println(t)
+	p := Point{X: 3, Y: 8}
+	showStruct(p)
 }
