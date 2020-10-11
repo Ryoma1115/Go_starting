720bac9fcb36b0b0746660ce022c7b79a178200c
github Ryoma1115 <t9831703@gmail.com>
2020-10-11 15:57:45 +0900

    Modified 5.4.go.

--- chapter5/5.4.go
+++ chapter5/5.4.go
@@ -1,21 +1,51 @@
+// package main
+
+// import "fmt"
+
+// type MyError struct {
+// 	Message string
+// 	ErrCode int
+// }
+
+// func (e *MyError) Error() string {
+// 	return e.Message
+// }
+
+// func RaiseError() error {
+// 	return MyError{Message: "エラーが発生しました。", ErrCode: 1234}
+// }
+
+// func main() {
+// 	err := RaiseError()
+// 	fmt.Println(err.Error())
+// }
 package main
 
 import "fmt"
 
-type MyError struct {
-	Message string
-	ErrCode int
+type I interface {
+	Method1() string
+	method2() string
 }
 
-func (e *MyError) Error() string {
-	return e.Message
+type T struct{}
+
+func (t *T) Method1() string {
+	return "*Method()"
 }
 
-func RaiseError() error {
-	return MyError{Message: "エラーが発生しました。", ErrCode: 1234}
+func (t *T) method2() string {
+	return "*method2()"
+}
+
+func NewI() I {
+	return &T{}
 }
 
 func main() {
-	err := RaiseError()
-	fmt.Println(err.Error())
+	t := main.NewI()
+	t.Method1()
+	t.method2()
+
+	fmt.Println(t)
 }
