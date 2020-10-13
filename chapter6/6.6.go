78c08617e241602b71f54bf61b8da0b8fc3d9496
github Ryoma1115 <t9831703@gmail.com>
2020-10-13 21:32:32 +0900

    Add 6.6.go.

---
+++ chapter6/6.6.go
@@ -0,0 +1,26 @@
+package foo
+
+const (
+	MAX = 100
+)
+
+const (
+	A = iota
+	B
+	C
+)
+
+type T struct {
+	Filed1 int
+	filed2 string
+}
+
+func (t *T) Method1() {
+}
+
+func (t *T) Method1() {
+}
+
+func New() *T {
+	return &T{}
+}
