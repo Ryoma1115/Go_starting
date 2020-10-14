271ecbce04b7d93639098db95835cd6a0b0a58a4
github Ryoma1115 <t9831703@gmail.com>
2020-10-14 15:28:59 +0900

    Modified 6.9.go.

--- chapter6/6.9.go
+++ chapter6/6.9.go
@@ -1,9 +1,21 @@
+// package foo
+
+// func IsOne(n int) bool {
+// 	if n == 1 {
+// 		return true
+// 	} else {
+// 		return false
+// 	}
+// }
+
 package foo
 
-func IsOne(n int) bool {
-	if n == 1 {
-		return true
-	} else {
-		return false
+import "testing"
+
+func TestIsOne(t *testing.T) {
+	n := 1
+	b := IsOne(n)
+	if b != true {
+		t.Errorf("%d is not one", n)
 	}
 }
