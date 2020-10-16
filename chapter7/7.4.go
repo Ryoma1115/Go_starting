32c889186800739b850d7adec895ae8fa983bfb7
github Ryoma1115 <t9831703@gmail.com>
2020-10-16 17:48:58 +0900

    Add 7.4.go.

---
+++ chapter7/7.4.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Pi())
+	fmt.Println(math.E())
+	fmt.Println(math.MaxFloat32())
+	fmt.Println(math.MaxUint16())
+	fmt.Println(math.Ceil(1.5))
+	fmt.Println(math.Pow(0, 2))
+}
