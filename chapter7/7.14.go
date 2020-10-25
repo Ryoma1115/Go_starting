396e0329a8c37764c185b98d1f3c580393f5a61a
github Ryoma1115 <t9831703@gmail.com>
2020-10-25 22:11:55 +0900

    Modified 7.14.go.

--- chapter7/7.14.go
+++ chapter7/7.14.go
@@ -1,18 +1,32 @@
+// // package main
+
+// // import (
+// // 	"io/ioutil"
+// // 	"log"
+// // 	"os"
+// // )
+
+// // func main() {
+// // 	f, err := os.Open("foo.txt")
+// // 	if err != nil {
+// // 		log.Fatal(err)
+// // 	}
+
+// // 	bs, err := ioutil.ReadAll(f)
+// // 	if err != nil {
+// // 		log.Fatal(err)
+// // 	}
+// // }
+
 // package main
 
 // import (
 // 	"io/ioutil"
 // 	"log"
-// 	"os"
 // )
 
 // func main() {
-// 	f, err := os.Open("foo.txt")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	bs, err := ioutil.ReadAll(f)
+// 	bs, err := ioutil.ReadFile("foo.txt")
 // 	if err != nil {
 // 		log.Fatal(err)
 // 	}
@@ -21,13 +35,20 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 )
 
 func main() {
-	bs, err := ioutil.ReadFile("foo.txt")
+	f, err := ioutil.TempFile(os.TempDir(), "foo")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer f.Close()
+
+	f.WriteString("Hello, World!\n")
+
+	fmt.Println(f.Name())
 }
